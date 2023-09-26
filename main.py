fc8fdf967dcc08ba967cd77d491d16b5410ac9b3
ALX <alexander.4ephb@gmail.com>
2023-09-26 20:03:52 +1000

    ADD: ыщsome func and routes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 ##########################################
 
 import os
-from flask import Flask, render_template, redirect, request, url_for, flash, session
+from flask import Flask, render_template, redirect, request, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -131,7 +131,6 @@ def home():
     if request.method == 'GET':
         names = db.session.execute(db.select(TNVDName).order_by(TNVDName.id)).scalars()
         print(*names)
-        # username = session.get('username')
         return render_template('home.html', current_user=current_user)
     else:
         read_xlsx_file()
@@ -141,11 +140,33 @@ def home():
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
+    global data
     if request.method == 'GET':
-        return render_template('edit.html')
+        return render_template('edit.html', data=data)
     elif request.method == 'POST':
-        save_xlsx_file()
-        return redirect(url_for('edit'))
+        if 'file' in request.files:
+            file = request.files['file']
+            if file.filename != '':
+                # Чтение данных из загруженного файла xlsx
+                excel_data = pd.read_excel(file)
+                # Отображение только необходимых столбцов
+                excel_data = excel_data[['Наименование', 'Торговая Марка', 'Количество, шт.', 'Вес БРУТТО, кг.']]
+                # Переименование столбцов
+                excel_data.columns = ['НАИМЕНОВАНИЕ2', 'ТМ', 'КОЛ-ВО', 'БР']
+                # Добавление данных в общую таблицу
+                if not excel_data.empty:
+                    data = pd.concat([data, excel_data], ignore_index=True)
+                    # data = pd.concat([data, excel_data.dropna(how='all')], ignore_index=True)
+                    # data = data.append(excel_data, ignore_index=True)
+
+            # Замена значений пустых ячеек с NaN на ''
+            data = data.fillna('')
+
+            # Применение функций чистки к столбцам
+            data['НАИМЕНОВАНИЕ2'] = data['НАИМЕНОВАНИЕ2'].apply(clean_1)  # тут чистка на наличие латиницы
+            data['ТМ'] = data['ТМ'].apply(clean_2)  # тут чистка на наличие кириллицы
+
+            return render_template('edit.html', data=data, columns=result_columns)
 
 
 @app.route('/reg', methods=['GET'])
@@ -158,25 +179,7 @@ def reg():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # name = request.form.get('name')
-    # password = request.form.get('password')
-    # if name and password:
-    #     user = User.query.filter_by(name=name).first()
-    #     if user and check_password_hash(user.password, password):
-    #         login_user(user)
-    #         next_page = request.args.get('next')
-    #         return redirect(next_page)
-    #     else:
-    #         print('Логин или пароль не верны')
-    #         flash('Логин или пароль не верны')
-    # else:
-    #     print('Пожалуйста, заполните поля логин и пароль')
-    #     flash('Пожалуйста, заполните поля логин и пароль')
-    #
-    # return render_template('login.html')
-
     form = forms.LoginForm()
-
     if request.method == 'GET':
         return render_template('login_2.html', form=form)
     else:
@@ -243,27 +246,45 @@ def page_not_found(error):
 # NEW #
 #######
 
-# @app.route('/table')
-# def table():
-#     data = request.args.get('data')
-#     file_path = request.args.get('file_path')
-#     df = pd.read_json(data)
-#     return render_template('table.html', data=df.to_dict('records'), file_path=file_path)
-#
-#
-# @app.route('/update_cell', methods=['POST'])
-# def update_cell():
-#     data = request.get_json()
-#     file_path = data['file_path']
-#     df = pd.read_excel(file_path)
-#     df.at[data['row'], data['column']] = data['value']
-#     save_excel(df, file_path)
-#     return 'success'
+@app.route('/update', methods=['POST'])
+def update():
+    global data
+    incoming = request.get_json()['data']
+    data = update_data(data, incoming)  # Обновляем данные в DataFrame
+    return jsonify({'status': 'OK'})
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    """
+    Сохранение данных в Excel файл и
+    Очистка рабочего датафрейма data
+    :return:
+    """
+    global data
+    data.to_excel('Result.xlsx', index=False)
+    # data = pd.DataFrame(columns=result_columns)
+    return redirect(url_for('index'))
 
 
 ##########################################
 # logic:
 ##########################################
+
+
+def update_data(dataframe, incoming_data):
+    """
+    Функция для обновления данных в DataFrame
+    :param dataframe:
+    :param incoming_data:
+    :return:
+    """
+    global data
+    updated_dataframe = pd.read_html(incoming_data)[0]
+    updated_dataframe.columns = [result_columns]
+    dataframe.update(updated_dataframe)
+    data = pd.concat([data, updated_dataframe], ignore_index=True, sort=False)
+    return data
 
 
 def clean_1(data):
@@ -286,15 +307,6 @@ def clean_2(data):
 
 def read_xlsx_file():
     print('reading xlsx..')
-    if request.method == 'POST':
-        file = request.files['file']
-        column_name = request.form['column_name']
-        if file:
-            file_path = 'uploads/' + file.filename
-            file.save(file_path)
-            df = process_excel(file_path, column_name)
-            return redirect(url_for('table', data=df.to_json(), file_path=file_path))
-    return render_template('home.html')
 
 
 def save_xlsx_file():
